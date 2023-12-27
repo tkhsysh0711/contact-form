2094967d2ff182234f4982752bcaa1f7161b437a
yoshi <ahg945127@gmail.com>
2023-12-27 16:47:32 +0900

    0-6

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
 use App\Models\Contact;
 
 class ContactController extends Controller
@@ -12,13 +12,13 @@ class ContactController extends Controller
         return view('index');
     }
 
-    public function confirm(Request $request)
+    public function confirm(ContactReuest $request)
     {
         $contact = $request->only(['name', 'email', 'tel', 'content']);
         return view('confirm', ['contact' => $contact]);
     }
 
-    public function store()
+    public function store(ContactRequest $request)
     {
         $contact = $request->only(['name', 'email', 'tel', 'content']);
         Contact::create($contact);
